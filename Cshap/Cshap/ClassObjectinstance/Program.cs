640ab8158753514ee94b4db7df6a4cb1741a953e
nojinmyoung <113429366+nojinmyoung@users.noreply.github.com>
2022-09-16 19:48:03 +0900

    orc class  example
    orc1 '상급오크'
    orc2 ' 하급오크'

--- Cshap/Cshap/ClassObjectinstance/Program.cs
+++ Cshap/Cshap/ClassObjectinstance/Program.cs
@@ -9,19 +9,47 @@ namespace ClassObjectinstance
         {
             // new 키워드
             // 동적할당 키워드
+            // 객체화 : 클래스타입의 멤버들만큼의 변수공간을 할당하는 과정
+            // 객체 : 클래스타입의 멤버들만큼 할당된 메모리 공간
+            // 인스턴스화 : 객체의 실제 데이터를 대입하는 과정
+            // 인스턴스 : 객체는 객체인데, 각 멤버들의 데이터가 초기화되어있는 상태의 객체
+
+            // C# 에서 생성자를 호출하는 과정이 객체화 이면서 인스턴스화 이다.
+            // 인스턴스 ⊂ 객체
             Human human = new Human();
-            int a = 3;
+
+            // .연산자 
+            // 멤버접근 연산자 
+            human.height = 150.0f;
+            human.name = "홍길동";
+            human.Breath();
+            Console.WriteLine(human.height);
+            Console.WriteLine($"성별 : {human.genderCharacter}");
+
+            // 객체의 멤버변수는 초기호값이 없을경우
+            // BBS 영역에 저장되며, 해당 영역은 모든비트가 0으로 세팅되기때문에
+            // 지역변수처럼 반드시 초기화를 할 필요가 없다.
+            // tempChar;
+            // Console.WriteLine($"성별 : {tempChar}");
+
         }
     }
     // 클래스도 사용자정의 '자료형'
     public class Human
     {
-        int age = 1;
-        float height; 
-        double weight;  
-        bool isReseting;
-        char genderCharacter;
-        string name;
+        // 보호수준을 결정하는 접근 제한자
+        // public : 접근 제한 없음
+        // private : 해당 객체 외 접근 제한
+        // protected : 해당객체 및 자식객체 외 접근 제한
+        // internal : 동일 어셈블리(프로젝트) 외 접근 제한
+        // class 의 멤버들에게 접근제한자를 명시하지 않을겨우
+        // private 이 기본값임.
+        public int age = 1;
+        public float height;
+        public double weight;
+        public bool isReseting;
+        public char genderCharacter;
+        public string name;
         
         // 생성자
         // 객체를 생성하는 함수
@@ -40,10 +68,20 @@ namespace ClassObjectinstance
 
         public Human()
         {
+            height = 160.0f;
+            weight = 300.0f;
+
+        }
+        
+        // 소멸자
+        // 해당 객체를 메모리에서 해제할때 호출하는 함수
+        // 가비지컬렉터가 행당 객체가 참조되지 않을경우 호줄해주기 때문에 직접 호출하는 일은 없다.
+        ~Human()
+        {
 
         }
 
-        void Breath()
+        public void Breath()
         {
             Console.WriteLine($"{name} (이)가 숨을 쉰다");
         }
---
+++ Cshap/Cshap/Example01_ClassObjectectlnstance/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+// Orc 클래스 작성 예제(1번)
+// Orc 는
+// 이름, 키, 몸무게, 나이 성별문자, 쉬고있는지아닌지?, 에 대한 특징을 가지고 있으며 (멤버변수)
+// 휘두르기 (smach) 점프하기(Jump) 라는 가능(멤버함수) 를 가지고 있습니다
+// Orc 클래슬르 작성한 후
+// 1. 
+// Main 함수 내에서 Orc 객체를 두개 만듭니다.
+// 첫번째 객체는
+// 이름 : 상급오크
+// 키 : 240.2
+// 몸무게 : 200.0
+// 나이 : 140
+// 성별문자 : '남'
+// 쉬고있는지 : x(false
+//
+// 두번째 객체
+// 이름 : 하급오크 
+// 키 : 140.4
+// 몸무게 : 120.0
+//나이 : 60
+// 성별문자 : '여'
+// 쉬고있는? : true
+//
+// 2. 각 오크들이 쉬고있는지 체크한 후 쉬고있는 오크에게 점프와 휘두르기를 시전시킵니다
+// (멤버함수 호줄)
+namespace Example01_ClassObjectectlnstance
+{
+
+    // Naming convention 
+    // PascalCase
+    // camelCase
+    // snake_case
+    // UPPER_SNAKE_CASE
+    //
+    // 클래스이름, 함수이름 : Pascal_Case
+    // public, protected 변수 : PascalCase
+    // private 변수 : _camelCase
+    // 지역변수 : camelCase
+    // 상수 : UPPER_SNAKE_CASE / snake_case
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc orc1 = new Orc();
+            
+            orc1.name = "상급오크";
+            orc1.age = 140;
+            orc1.height = 240.2f;
+            orc1.weightl = 400.0;
+            orc1.genderCharacter = '남';
+            orc1.IsReseting = false;
+
+            Orc orc2 = new Orc();
+            orc2.name = "하급오크";
+            orc2.age = 60;
+            orc2.height = 140.4f;
+            orc2.weightl = 120.0;
+            orc2.genderCharacter = '여';
+            orc2.IsReseting = true;
+
+            orc1.Jump();
+            orc2.Smach();
+
+        }
+    }
+    public class Orc
+    {
+        private bool _isDateSaved;
+        public int age;
+        public float height;
+        public double weightl;
+        public bool IsReseting;
+        public char genderCharacter;
+        public string name;
+
+        public void Smach()
+        {
+            Console.WriteLine($"{name} (이)가 휘둘렀다...!");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine($"{name} (이)가 점프했다...!");
+        }
+
+
+
+    }  
+
+}
