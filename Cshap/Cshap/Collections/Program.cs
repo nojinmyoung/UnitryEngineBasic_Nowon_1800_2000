2e00d80cedf56b2e6acbae7379730d1dc587de89
nojinmyoung <113429366+nojinmyoung@users.noreply.github.com>
2022-10-04 19:51:16 +0900

    MyDynamicAray
    MyDynamicAray

--- Cshap/Cshap/Collections/Program.cs
+++ Cshap/Cshap/Collections/Program.cs
@@ -21,7 +21,8 @@ namespace Collections
             // Collections
             //----------------------------------------------------------------------------------------------------
 
-
+            // ArrayList
+            // C# 의 동적배열
             ArrayList arrayList = new ArrayList();
             arrayList.Add(3); // 3을 박싱 후 해당 참조를 추가한다.
             // ArrayList 는 object[] 에다가 데이터를 저장한다.
@@ -42,7 +43,7 @@ namespace Collections
             stack.Pop();
 
             // Queue
-            // FIFO (First Input First Output
+            // FIFO (First Input First Output)
             Queue queue = new Queue();
             queue.Enqueue(3); // 아이템 젤 앞에 추가
             queue.Enqueue(4);
@@ -66,7 +67,7 @@ namespace Collections
             list.Add(3);
             list.Add(4);
             list.Remove(3);
-            list.RemoveAt(0);
+            list.RemoveAt(0); 
 
             // LinkedList<T>
             // C# 의 Doubly-연결리스트 일반형
@@ -74,6 +75,43 @@ namespace Collections
             linkedList.AddLast(3.0f);
             linkedList.Remove(3.0f);
             linkedList.RemoveFirst();
+
+            // Queue<T>
+            Queue<bool> boolQueue = new Queue<bool>();
+            boolQueue.Enqueue(true);
+            boolQueue.Enqueue(false);
+
+            // Stack<T>
+            Stack<double> doubleStack = new Stack<double>();
+            doubleStack.Push(1.0f);
+            doubleStack.Pop();
+
+            // Dictionary<T>
+            // Hashtable 의 제네릭 타입
+            // 중북 키를 Add시 에러
+            Dictionary<string, float> points = new Dictionary<string, float>();
+
+            // key 존재하는지 검색(인덱서에 Hash("철수") 넣고 예외 던져지는지 체크) 
+            if (points.ContainsKey("철수") == false)
+            {
+                points.Add ("철수, 1.0f");
+            }
+
+            // key 존재하는지 검색(인덱서에 Hash("철수") 넣고 예외 던져지는지 체크)  안던져지면 out 파라미터에 value 반환
+            if (points.TryGetValue("철수", out float value))
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine(points["철수"]);
+            points.Add("철수", 1.0f);
+            Console.WriteLine(points["철수"]);
+
+            // HashSet<T>
+            // 중복을 허용하지않는 동적배열 (해시의 집합을 표현하기위한 자료구조)
+            // 중복 키를 Add시 그냥 무시
+            HashSet<int> intset = new HashSet<int>();
+            intset.Add(1);
+            intset.Add(2);
 
 
         }
---
+++ Cshap/Cshap/MyDynamicAray/MyDynamicAray.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDynamicAray
+{
+    internal class MyDynamicAray
+    {
+        private const int DEFAULT_SIZE = 1;
+        private int[] _data = new int[DEFAULT_SIZE];
+        public int this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+
+            private set
+            {
+                _data[index] = value;
+            }
+        }
+
+        public int Count;
+        public int Capacity;
+
+        public void Add(int item)
+        {
+            // 배열의 크키가 모자라면
+            if (Count >= Capacity)
+            {
+                Capacity * = 2;
+                // 2배짜리 새로운 배열 생성
+                int[] tmp = new int[Capacity];
+
+                // 기존 데이터를 새로운 배열에 복제          
+                for (int i = 0; i < Count; i++)
+                {
+                    tmp[i] = _data[i];
+                }
+
+                // 새로운 배열로 참조 변경
+                _data = tmp;
+            } 
+
+            _data[Count] = item;
+            Count++;
+
+        }
+
+        P
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       //public int Mp { get; private set; }
+       //
+       //
+       //public int Count;
+       //public int Capacity;
+       //
+       //private int _hp;
+       //
+       //public int Hp
+       //{
+       //    get
+       //    {
+       //        return _hp;
+       //    }
+       //
+       //    set
+       //    {
+       //        if (value < 0)
+       //            value = 0;
+       //
+       //        _hp = value;
+       //        _hpBar.value = _hp / _hpMax;
+       //    }
+       //}
+       //
+       //
+       //private UISlider _hpBar;
+       //public int _hpMax;
+       //
+       //public int GetHp()
+       //{
+       //    return _hp;
+       //}
+       // 
+       //public void SetHp(int value)
+       //{
+       //    if(value < 0)
+       //       value = 0;
+       //
+       //    _hp = value;
+       //    _hpBar.value = _hp / _hpMax;
+       //}
+       //
+       //
+   // class UISlider { public float value;  }
+
+    }
+}
