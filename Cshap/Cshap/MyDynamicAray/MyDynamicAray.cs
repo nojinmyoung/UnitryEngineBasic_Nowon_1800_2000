2889f05a1aeed847bd337aa308ad9dc21f0d203c
nojinmyoung <113429366+nojinmyoung@users.noreply.github.com>
2022-10-05 19:37:42 +0900

    MyDynamicAray
    MyDynamicAray

--- Cshap/Cshap/MyDynamicAray/MyDynamicAray.cs
+++ Cshap/Cshap/MyDynamicAray/MyDynamicAray.cs
@@ -24,16 +24,17 @@ namespace MyDynamicAray
         }
 
         public int Count;
-        public int Capacity;
+        public int Capacity => _data.Length;    // get 접근자의 람다식 표현, 람다식 : 익명함수 표현방법 
+       
 
         public void Add(int item)
         {
             // 배열의 크키가 모자라면
             if (Count >= Capacity)
             {
-                Capacity * = 2;
+                
                 // 2배짜리 새로운 배열 생성
-                int[] tmp = new int[Capacity];
+                int[] tmp = new int[Capacity * 2];
 
                 // 기존 데이터를 새로운 배열에 복제          
                 for (int i = 0; i < Count; i++)
@@ -50,78 +51,106 @@ namespace MyDynamicAray
 
         }
 
-        P
 
+        /// <summary>
+        /// 특정 인덱스에 있는 데이터를 지우고
+        /// 그 뒤 모든 인덱스 데이터들을 한칸씩 앞으로 당겨줌
+        /// </summary>
+        /// <param name="index">지우고싶은 데이터의 인덱스</param>
+        /// <returns>ture : 삭제됨, false : 삭제안됨 </returns>
+        public bool RemoveAt(int index)
+        {
+            if(index < 0  || index > Count -1)
+                return false;
 
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
 
+            Count--;
+            _data[Count] = 0;
+            return true;
+        }
 
+        public bool Remove(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_data[i] == item)
+                    return RemoveAt(i);
+            }
+            return false;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       //public int Mp { get; private set; }
-       //
-       //
-       //public int Count;
-       //public int Capacity;
-       //
-       //private int _hp;
-       //
-       //public int Hp
-       //{
-       //    get
-       //    {
-       //        return _hp;
-       //    }
-       //
-       //    set
-       //    {
-       //        if (value < 0)
-       //            value = 0;
-       //
-       //        _hp = value;
-       //        _hpBar.value = _hp / _hpMax;
-       //    }
-       //}
-       //
-       //
-       //private UISlider _hpBar;
-       //public int _hpMax;
-       //
-       //public int GetHp()
-       //{
-       //    return _hp;
-       //}
-       // 
-       //public void SetHp(int value)
-       //{
-       //    if(value < 0)
-       //       value = 0;
-       //
-       //    _hp = value;
-       //    _hpBar.value = _hp / _hpMax;
-       //}
-       //
-       //
-   // class UISlider { public float value;  }
-
+       // public IEnumerator<int GetEnumerator()
+       // {
+       //     
+       // }
+       
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//프로퍼티 ?
+
+//private int _hp;
+//
+//public int Hp
+//{
+//    get
+//    {
+//        return _hp;
+//    }
+//
+//    private set
+//    {
+//        if (value < 0)
+//            value = 0;
+//
+//        _hp = value;
+//        _hpBar.value = _hp / _hpMax;
+//    }
+//}
+//
+//public int Mp { get; private set; }
+//
+//
+//private UISlider _hpBar;
+//private int _hpMax;
+//
+//public int GetHp()
+//{
+//    return _hp;
+//}
+//
+//public void SetHp(int value)
+//{
+//    if (value < 0)
+//        value = 0;
+//
+//    _hp = value;
+//    _hpBar.value = _hp / _hpMax;
+//}
+//
+//class UISlider { public float value; }
--- Cshap/Cshap/MyDynamicAray/Program.cs
+++ Cshap/Cshap/MyDynamicAray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyDynamicAray
 {
@@ -6,7 +7,49 @@ namespace MyDynamicAray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          List<int> list = new List<int>();
+            list.Add(1);   
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Remove(1);
+            Console.WriteLine(list[0]);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator<int> eumaerator = list.GetEnumerator();
+
+            while (eumaerator.MoveNext())
+            {
+                Console.WriteLine(eumaerator.Current);
+            }
+
+            MyDynamicAray dynamicAray = new MyDynamicAray();
+            dynamicAray.Add(1); 
+            dynamicAray.Add(2);
+            dynamicAray.Add(3);
+            dynamicAray.Add(4);
+            dynamicAray.Remove(1);
+            Console.WriteLine(dynamicAray[0]);
+
+
+            for (int i = 0; i < dynamicAray.Count; i++)
+            {
+                Console.WriteLine(dynamicAray[i]);
+            }
+
+           // foreach (int item in dynamicAray)
+           // {
+           //
+           // }
         }
     }
 }
